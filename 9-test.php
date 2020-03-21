f1c4d5476c55f4bd9504bdd889abc4f6430e0108
codeaid-dev <codeaid.lab@gmail.com>
2020-03-21 13:45:04 +0900

    add files

--- 9-test.php
+++ 9-test.php
@@ -13,3 +13,20 @@ $contents = file_get_contents("foods.txt");
 $foods = explode("\n", $contents);
 $dsp = implode(",", $foods);
 print($dsp . "\n");
+
+
+$fh = fopen("menu.txt", "rb");
+$menu = array();
+while ((!feof($fh)) && ($line = fgets($fh))) {
+  $line = trim($line);
+  $info = explode(',', $line);
+  $menu[$info[0]] = $info[1];
+}
+fclose($fh);
+
+$fh = fopen("menu-tax.txt", "wb");
+foreach ($menu as $key => $value) {
+  $menu[$key] += $value*0.1;
+  fwrite($fh, "$key,$menu[$key]\n");
+}
+fclose($fh);
