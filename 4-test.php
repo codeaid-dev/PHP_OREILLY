4391c4d07956d6e0d10cd250835d34d4a9906071
codeaid-dev <codeaid.lab@gmail.com>
2020-03-27 21:20:49 +0900

    add fix some files

--- 4-test.php
+++ 4-test.php
@@ -1,10 +1,21 @@
 <?php
-// 配列　解答
-// ********** Q1 **********
+// 配列　解答例
+// ********** Q1-1 **********
 $teams = ["team-A", "team-B", "team-C", "team-D", "team-E"];
 for ($i = 0; $i < count($teams); $i++) {
   for ($j = $i+1; $j < count($teams); $j++) {
     print($teams[$i] . " vs " . $teams[$j] . "\n");
+  }
+}
+
+
+// ********** Q1-2 **********
+$teams = ["team-A", "team-B", "team-C", "team-D", "team-E"];
+$vss = ["team-A", "team-B", "team-C", "team-D", "team-E"];
+foreach ($teams as $t1) {
+  array_shift($vss);
+  foreach ($vss as $t2) {
+    print($t1 . " vs " . $t2 . "\n");
   }
 }
 
