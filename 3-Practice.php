8fa0502dd6e4e6fcc7d4c6d7804c1a7bdd395ef8
codeaid-dev <codeaid.lab@gmail.com>
2020-07-22 16:43:45 +0900

    fix

--- 3-Practice.php
+++ 3-Practice.php
@@ -212,3 +212,28 @@ if ($person == "male") {
 } else {
   print "判定できませんでした\n";
 }
+
+// ********** Q21 **********
+print "入力：";
+$str = trim(fgets(STDIN));
+$flag = false;
+$start = 0;
+$end = 0;
+for ($i = 0; $i < strlen($str); $i++) {
+  $ch = substr($str, $i, 1);
+  if ($ch == "(") {
+    $start = $i;
+    $flag = true;
+  } else if ($ch == ")" && $flag == true) {
+    $end = $i;
+    $replace = "";
+    $len = $end-$start+1;
+    $search = substr($str, $start, $len);
+    for ($j = 0; $j < $len-2; $j++) {
+      $replace .= "x";
+    }
+    $replace = "(" . $replace . ")";
+    $str = str_replace($search, $replace, $str);
+  }
+}
+print "結果表示：" . $str . "\n";
